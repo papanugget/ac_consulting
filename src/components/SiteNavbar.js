9662d56aaa69efc7dcb6e62c1c566299e7a47b0c
papanugget <evilgenius04@gmail.com>
2019-09-05 11:02:53 -0400

    sitenav links added / collapse works

--- src/components/SiteNavbar.js
+++ src/components/SiteNavbar.js
@@ -4,59 +4,73 @@ import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } f
 import '@fortawesome/free-brands-svg-icons';
 import { faInstagram, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-const SiteNavbar = () => {
-    return (
-        <div>
-            <Navbar color="faded" light fixed expand="lg">
-                <NavbarBrand href="/" className="mr-auto">Antoine Cole Consulting</NavbarBrand>
-                    <NavbarToggler className="mr-2" />
-                    <Collapse navbar isOpen="true">
-                        <Nav className="ml-auto" navbar>
-                        <NavItem>
-                            <NavLink href="/">
-                                Home
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/services/">
-                                Services
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink>
-                                Gallery
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink>
-                                Contact
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink>
-                                About
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="https://instagram.com/come_stay_at_the_rockaways" target="_blank">
-                                <FontAwesomeIcon icon={faInstagram}/>
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="https://twitter.com" target="_blank">
-                                <FontAwesomeIcon icon={faTwitter}/>
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="https://linkedin.com" target="_blank">
-                                <FontAwesomeIcon icon={faLinkedin}/>
-                            </NavLink>
-                        </NavItem>
-                        </Nav>
-                    </Collapse>
-            </Navbar>
-        </div>
-    )
+class SiteNavbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.toggle =  this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+         };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar color="faded" light fixed expand="md">
+                    <NavbarBrand href="/" className="mr-auto">Antoine Cole Consulting</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle}/>
+                        <Collapse isOpen={this.state.isOpen}  navbar>
+                            <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/">
+                                    Home
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/services/">
+                                    Services
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/gallery/">
+                                    Gallery
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/contact/">
+                                    Contact
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/about/">
+                                    About
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="https://instagram.com/come_stay_at_the_rockaways" target="_blank">
+                                    <FontAwesomeIcon icon={faInstagram}/>
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="https://twitter.com" target="_blank">
+                                    <FontAwesomeIcon icon={faTwitter}/>
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="https://linkedin.com" target="_blank">
+                                    <FontAwesomeIcon icon={faLinkedin}/>
+                                </NavLink>
+                            </NavItem>
+                            </Nav>
+                        </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
 }
 
 export default SiteNavbar;
