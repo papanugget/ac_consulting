5f18e655491fb24cd0f58c15675142a536ad3c55
papanugget <evilgenius04@gmail.com>
2019-09-05 08:24:06 -0400

    sitenavbar work

--- src/components/SiteNavbar.js
+++ src/components/SiteNavbar.js
@@ -3,24 +3,53 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import '@fortawesome/free-brands-svg-icons';
 // import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-import { faInstagram, faTwitterSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const SiteNavbar = () => {
     return (
         <div className="col-xs-12 col-sm-10 col-md-10 col-lg-10">
-            <Navbar color="faded" light>
+            <Navbar color="faded" light expand="sm">
                 <NavbarBrand href="/" className="mr-auto">Antoine Cole Consulting</NavbarBrand>
-                <NavbarToggler className="mr-2" />
-                <Collapse navbar>
-                    <Nav navbar>
-                    <NavItem>
-                        <NavLink href="/components/">Components</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-                    </NavItem>
-                    </Nav>
-                </Collapse>
+                 <NavbarToggler className="mr-2" />
+                    <Collapse navbar isOpen="true">
+                        <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink>
+                                Home
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink>
+                                Gallery
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink>
+                                Contact
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink>
+                                About
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://instagram.com/come_stay_at_the_rockaways" target="_blank">
+                                <FontAwesomeIcon icon={faInstagram}/>
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://twitter.com" target="_blank">
+                                <FontAwesomeIcon icon={faTwitter}/>
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://linkedin.com" target="_blank">
+                                <FontAwesomeIcon icon={faLinkedin}/>
+                            </NavLink>
+                        </NavItem>
+                        </Nav>
+                    </Collapse>
             </Navbar>
         </div>
     )
