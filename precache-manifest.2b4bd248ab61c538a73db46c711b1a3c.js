2234a0346f06c2fcd75c85fd570bc9c467c1b194
papanugget <evilgenius04@gmail.com>
2019-09-13 16:09:04 -0400

    Updates

--- precache-manifest.2b4bd248ab61c538a73db46c711b1a3c.js
+++ precache-manifest.2b4bd248ab61c538a73db46c711b1a3c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "664ec70a62de208ec1922a38a880e7ba",
+    "revision": "057958cd90b0a955fbd59a7199efd30e",
     "url": "/ac_consulting/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ac_consulting/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "23770d369efe2ccfb45a",
+    "revision": "e8fc83503f29b4329fd0",
     "url": "/ac_consulting/static/css/main.694fdb8a.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ac_consulting/static/js/2.ea4f9558.chunk.js"
   },
   {
-    "revision": "23770d369efe2ccfb45a",
-    "url": "/ac_consulting/static/js/main.0a11e59f.chunk.js"
+    "revision": "e8fc83503f29b4329fd0",
+    "url": "/ac_consulting/static/js/main.b57900cd.chunk.js"
   },
   {
     "revision": "84ac3e0529c3d65cdc24",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ac_consulting/precache-manifest.6ed04b33a02b4b8f4f16134dd15a43c1.js"
+  "/ac_consulting/precache-manifest.2b4bd248ab61c538a73db46c711b1a3c.js"
 );
 
 self.addEventListener('message', (event) => {
